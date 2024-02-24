493eebbb3a9ffca8e35d70b81d214158179fd6b1
R1nge <r1ngegamedev@gmail.com>
2024-02-24 13:09:04 +0300

    Dependencies DI

---
+++ app/src/main/java/com/r1nge/todo/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.r1nge.todo.di
+
+import android.app.Application
+import androidx.room.Room
+import com.r1nge.todo.data.TodoDatabase
+import com.r1nge.todo.data.TodoRepository
+import com.r1nge.todo.data.TodoRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            "todo_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.dao)
+    }
+}
